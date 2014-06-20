589f1a9bcf94544e6994aed625d06fe7ec708ce7
Raphael Catolino <raphael.catolino@gmail.com>
2014-06-20 22:07:21 +0200

    Jmp section.

--- disasm.py
+++ disasm.py
@@ -2,11 +2,32 @@
 
 import argparse
 
-def dis_line(leftovers, code):
-  opcodes = [code[i:i+4] for i in range(0, len(code)-1, 4)]
-  for opc in opcodes:
-    print(opcodes, leftovers)
-  return (opcodes, '')
+oneop_flag = 0xfc
+oneop_kind = 0x10
+
+jmp_kind = 0x20
+jmp_flag = 0xe0
+jmp_cond = 0x1c
+jmp_pc = 0x3
+jmp = ['jnz', 'jz', 'jnc', 'jc', 'jn', 'jge', 'jl', 'jmp']
+
+def jump(opc):
+  cond = (opc[0] & jmp_cond) >> 2
+  pc = 2 * ((opc[0] & jmp_pc << 8) + opc[1]) + 2
+  return "{} pc{:+#x}".format(jmp[cond], pc)
+
+def dis_line(leftovers, code, offset):
+  opcodes = [[int(code[i+2:i+4], 16), int(code[i:i+2], 16)] for i in range(0, len(code)-1, 4)]
+  i = 0
+  while i != len(opcodes):
+    next = opcodes[i]
+    if next[0] & jmp_flag == jmp_kind:
+      print("{:#x}:\t{}".format(offset + 2*i, jump(next)))
+#    if next[0] == oneop_kind:
+#      return oneop(next, opcodes[i+1])
+    i += 1
+
+  return (leftovers, '')
 
 def get_args():
   parser = argparse.ArgumentParser('Disassemble an msp430 binary')
@@ -34,8 +55,8 @@ def main():
     if offset - loffset > 0x10:
       continue
     elif loffset <= offset:
-      (leftovers, disassembly) = dis_line(leftovers, code[offset - loffset:])
+      (leftovers, disassembly) = dis_line(leftovers, code[offset - loffset:], offset)
     else:
-      (leftovers, disassembly) = dis_line(leftovers, code)
+      (leftovers, disassembly) = dis_line(leftovers, code, loffset)
 
 main()
